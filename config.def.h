7b229e994b81133efbef67d8a2757d680554d048
Nicolas Dietrich <ndietric@mathematik.uni-kl.de>
2023-01-19 14:54:53 +0100

    fontsize

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 300;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"SF Mono:size=11:antialias=true:autohint=true"
+	"SF Mono:size=13:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
